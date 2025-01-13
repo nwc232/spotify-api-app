e2e9d37b762d24c9fc7a5b49732f5dfbd2c88bc5
nwc232 <nwc836@gmail.com>
2025-01-13 01:32:56 -0800

    Fixed redirect uri

--- src/SpotifyAuth.js
+++ src/SpotifyAuth.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const CLIENT_ID = '5de5690a75c14ddcbf960a1def348244';
-const REDIRECT_URI = 'http://localhost:3000/callback';
+const REDIRECT_URI =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/callback'
+    : 'https://spotify-api-app-chi.vercel.app/callback';
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
 
